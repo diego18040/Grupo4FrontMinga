70ecb67b8ea511c43a2ea5e4bc0a1d7011e9aa51
Familia QM <felqueu@gmail.com>
2024-11-29 16:28:24 -0600

    actualización outlet

---
+++ src/Layouts/PanelLayout.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+
+
+
+export default function PanelLayout() {
+    return (
+        <>
+            <div className="min-h-screen flex flex-col bg-yellow-400">
+                <div className="bg-black">
+
+
+                </div>
+                <main className="flex-1 mt-1">
+                    <Outlet />
+                </main>
+
+            </div>
+        </>
+    );
+}
